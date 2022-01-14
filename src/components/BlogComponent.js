9d1dc91dc416353099e06717b33e345ae63d31a9
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-14 21:41:56 +0530

    Added Blog content to blog page 👍
    1. Changed image
    2. Blog data fetch from the api
    3. Blog's links working
    4. Done hovering effects

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,18 +1,85 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Box = styled(NavLink)`
-  width: calc(10rem + 15vw);
-  text-decoration: none;
   height: 20rem;
+  width: calc(10rem + 15vw);
   padding: 1rem;
+  text-decoration: none;
+  cursor: pointer;
+
+  color: ${props => props.theme.body};
+  border-radius: 10px;
+  border: 2px solid ${props => props.theme.body};
+  backdrop-filter: blur(15px);
+  box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
+
+  display: flex;
+  flex-direction: column;
+  z-index: 5;
+
+  &:hover{
+    color: ${props => props.theme.text};
+    background-color: #ffffff;
+    transition: all 0.3s ease;
+  }
 `
 
-const BlogComponent = () => {
+const Image = styled.div`
+  background-image: ${props => `url(${props.img})`};
+  width: 100%;
+  height: 60%;
+  background-size: cover;
+  border-radius: 10px;
+  border: 2px solid #ffffff;
+  background-position: center center;
+
+  ${Box}:hover &{
+    border: 2px solid ${props => props.theme.text};
+  }
+`
+
+const Title = styled.h3`
+  color: inherit;
+  padding: 0.5rem 0;
+  padding-top: 1rem;
+  font-family: 'Vogue', serif;
+  font-weight: 500;
+  margin-top: 1rem;
+  border-bottom: 3px solid ${props => props.theme.body};
+`
+
+const HashTags = styled.div`
+  padding: 0.5rem 0;
+`
+
+const Tag = styled.span`
+  padding-right: 0.5rem;
+`
+
+const Date = styled.div`
+  padding: 0.5rem 0;
+`
+
+const BlogComponent = (props) => {
+  const { id, name, tags, date, imgSrc, link } = props.blog;
+
   return (
-    <Box to="/">
-      Blog Items
+    <Box target="_blank" to={{ pathname: link }}>
+      <Image img={imgSrc}></Image>
+      <Title>{id}. {name}</Title>
+      <HashTags>
+        {
+          tags.map((tag, id) => {
+            return <Tag key={id}>#{tag}</Tag>
+          })
+        }
+      </HashTags>
+      <Date>
+        {date}
+      </Date>
     </Box>
   )
 }
--- src/components/blog.js
+++ src/components/blog.js
@@ -16,7 +16,7 @@ const MainContainer = styled.div`
 `
 
 const Container = styled.div`
-  background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
+  background-color: ${props => `rgba(${props.theme.bodyRgba},0.3)`};
   position: relative;
   width: 100%;
   height: auto;
@@ -34,7 +34,6 @@ const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
   grid-gap: calc(1rem + 2vw);
-
 `
 
 const blog = () => {
@@ -46,7 +45,7 @@ const blog = () => {
         <SocialIcons></SocialIcons>
         <Center>
           <Grid>
-            {Blogs.map(blogs => {
+            {Blogs.map(blog => {
               return <BlogComponent key={blog.id} blog={blog}></BlogComponent>
             })}
           </Grid>
