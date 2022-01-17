b715cc4ea411ee429fae911ba9dbd64df05ce3e7
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-17 19:55:29 +0530

    AboutPage Background setup 😎🚀
    just stars particle config

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { motion } from 'framer-motion'
+import { lightTheme } from './themes'
+import LogoComp from '../subComponents/LogoComp'
+import SocialIcons from '../subComponents/SocialIcons'
+import HomeButton from '../subComponents/HomeButton'
+import ParticleComponentAbout from '../Particles/ParticleComponentAbout'
+// import ImgParticles from '../Particles/ImgParticles'
+// import Particles from 'react-particles-js'
 
-const about = () => {
+const Box = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  ${'' /* background-color: ${props => props.theme.body}; */}
+  
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  
+`
+
+const AboutPage = () => {
   return (
-    <div>
-      About components/Page
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}>
+        <LogoComp></LogoComp>
+        <SocialIcons></SocialIcons>
+        <HomeButton></HomeButton>
+        <Box>
+
+        </Box>
+        <ParticleComponentAbout></ParticleComponentAbout>
+      </motion.div>
+    </ThemeProvider>
   )
 }
 
-export default about
+export default AboutPage
