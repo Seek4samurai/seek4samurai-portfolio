4fddbaa01f5bd7898f83ccf256c88539401a4c38
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-19 10:14:14 +0530

    projects-page setup 🌏

--- src/components/works.js
+++ src/components/works.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import HomeButton from '../subComponents/HomeButton'
+import SocialIcons from '../subComponents/SocialIcons'
+import { lightTheme } from './themes'
+import { Work } from '../data/WorkData'
+import Card from '../subComponents/Card'
+import img from '../assets/Images/ProjectBG.jpg'
 
-const Works = () => {
+const Box = styled.div`
+  position: relative;
+
+  height: 400vh;
+  background-color: #000;
+  background-color: ${props => props.theme.body};
+
+  overflow: hidden;
+
+  background-image: url(${img});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+`
+
+const Main = styled.ul`
+  position: fixed;
+  top: 12rem;
+  left: calc(10rem + 15vw);
+  height: 40vh;
+
+  display: flex;
+`
+
+const WorkPage = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    let element = ref.current;
+
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`
+    }
+
+    window.addEventListener('scroll', rotate)
+    return () => {
+      window.removeEventListener('scroll', rotate)
+    }
+  }, [])
+
   return (
-    <div>
-      Works components/Page
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0 }}>
+        <SocialIcons></SocialIcons>
+        <HomeButton></HomeButton>
+        <Box>
+          <Main>
+            {
+              Work.map(data =>
+                <Card key={data.id} data={data}></Card>
+              )
+            }
+          </Main>
+        </Box>
+      </motion.div>
+    </ThemeProvider>
   )
 }
 
-export default Works
+export default WorkPage
---
+++ src/subComponents/Card.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Github } from '../data/AllSvgs';
+
+const Box = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  
+  background-color: ${props => props.theme.body};
+  color: ${props => props.theme.text};
+
+  width: 16rem;
+  height: 50vh;
+  padding: 1.5rem 2rem;
+  margin-right: 5rem; 
+
+  border-radius: 15px;
+  border: 2px solid ${props => props.theme.text};
+
+  transition: all 0.2s ease;
+  &:hover{
+  color: ${props => props.theme.body};
+  background-color: ${props => props.theme.text};
+  border: 2px solid ${props => props.theme.body};
+  }
+`
+
+const Title = styled.h2`
+  font-size: calc(1em + 0.5vw);
+`
+const Description = styled.h2`
+  font-size: calc(0.8em + 0.3vw);
+  font-family: 'Karla', sans-serif;
+  font-weight: 500;
+`
+
+const Tags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  border-top: 2px solid ${props => props.theme.text};
+  padding-top: 0.5rem;
+
+  ${Box}:hover &{
+  border-top: 2px solid ${props => props.theme.body};
+  }
+`
+
+const Tag = styled.span`
+  font-size: calc(0.8em + 0.3vw);
+  margin-right: 1rem;
+`
+
+const Footer = styled.footer`
+  display:flex;
+  justify-content: space-between;
+`
+
+const Git = styled(NavLink)`
+  color: inherit;
+  text-decoration: ${props => props.theme.body};
+  ${Box}:hover &{
+    &>*{
+      fill: ${props => props.theme.text};
+    }
+  }
+`
+
+const Card = (props) => {
+  const { id, name, description, tags, github } = props.data;
+
+  return (
+    <Box key={id}>
+      <Title>
+        🚀{name}
+      </Title>
+      <Description>
+        🌏{description}
+      </Description>
+      <Tags>{
+        tags.map((tag, id) => {
+          return <Tag key={id}>#{tag}</Tag>
+        })
+      }
+      </Tags>
+      <Footer>
+        <Git to={{ pathname: `${github}` }} target="_blank">
+          <Github width={30} height={30}></Github>
+        </Git>
+      </Footer>
+    </Box>
+  )
+}
+
+export default Card
