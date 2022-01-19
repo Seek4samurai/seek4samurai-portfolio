8913b24c7b2ce387a2e389ae63c7e9c82be9d4df
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-19 10:15:08 +0530

    converted workdata api to export as Work Comp 😎🚀

---
+++ src/data/WorkData.js
@@ -0,0 +1,36 @@
+export const Work = [
+    {
+        id: 1,
+        name: "The Wholesome App",
+        description: "Built on top of Express, Hbs and Atlas. This application allows you to upload your cute images directly to Atlas Database.",
+        tags: ["express", "nodeJS", "atlas"],
+
+        github: "https://github.com/Seek4samurai/The-Wholesome-App"
+    },
+    {
+        id: 2,
+        name: "Web Games",
+        description: "Website featuring two games made out of Canvas and Vanilla Javascript.",
+        tags: ["js", "sass", "canvas"],
+
+        github: "https://github.com/Seek4samurai/web-games/"
+
+    },
+    {
+        id: 3,
+        name: "Craigslist Clone",
+        description: "A clone of Craigslist build using Django framework. With realtime fetching of data from Craigslist using BS4.",
+        tags: ["django", "api", "beautifulSoup"],
+
+        github: "https://github.com/Seek4samurai/craigslist-clone-app"
+    },
+    {
+        id: 4,
+        name: "Data Structures and Algorithms",
+        description: "Repository full of solved data structures and algorithm questions.",
+        tags: ["algorithms", "cpp", "dsa"],
+
+        github: "https://github.com/Seek4samurai/DSA-Cpp"
+
+    },
+] 
