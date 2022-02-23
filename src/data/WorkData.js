fe0872b3aa4b8a0c8247def5c8dcd8791d4eb2e4
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-02-23 15:08:55 +0530

    added content to workdata

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -9,6 +9,22 @@ export const Work = [
     },
     {
         id: 2,
+        name: "Azura",
+        description: "Build and powered by Microsoft Azure. This can be used as both web app, and a browser extension. Using Azure's computer vision API it'll show the description about an image",
+        tags: ["azure", "computerVision", "ai"],
+
+        github: "https://github.com/Seek4samurai/azura"
+    },
+    {
+        id: 3,
+        name: "Azura Website",
+        description: "Build and powered by Microsoft Azure. This is web application for using Azura. Using Azure's computer vision API it'll show the description about an image",
+        tags: ["azure", "computerVision", "ai"],
+
+        github: "https://github.com/Seek4samurai/azura-website"
+    },
+    {
+        id: 4,
         name: "Web Games",
         description: "Website featuring two games made out of Canvas and Vanilla Javascript.",
         tags: ["js", "sass", "canvas"],
@@ -17,7 +33,7 @@ export const Work = [
 
     },
     {
-        id: 3,
+        id: 5,
         name: "Craigslist Clone",
         description: "A clone of Craigslist build using Django framework. With realtime fetching of data from Craigslist using BS4.",
         tags: ["django", "api", "beautifulSoup"],
@@ -25,12 +41,19 @@ export const Work = [
         github: "https://github.com/Seek4samurai/craigslist-clone-app"
     },
     {
-        id: 4,
-        name: "Data Structures and Algorithms",
-        description: "Repository full of solved data structures and algorithm questions.",
-        tags: ["algorithms", "cpp", "dsa"],
+        id: 5,
+        name: "NexTrade(WIP)",
+        description: "Nft and artwork selling platform, that works on ethereum (WORK IN PROGRESS).",
+        tags: ["ethereum", "thirdweb", "nfts"],
 
-        github: "https://github.com/Seek4samurai/DSA-Cpp"
-
+        github: "https://github.com/Seek4samurai/NexTrade"
     },
+    {
+        id: 6,
+        name: "Seek4samurai Portfolio",
+        description: "The portfolio website you're looking at right now.",
+        tags: ["js", "tsparticles", "reactjs"],
+
+        github: "https://github.com/Seek4samurai/reactJS-portfolio"
+    }
 ] 
