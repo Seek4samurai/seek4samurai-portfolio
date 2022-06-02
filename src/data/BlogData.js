89f4a51dca53d9b5ae03110aee4689d129b962c1
Seek4samurai <grvrawat999@gmail.com>
2022-06-02 22:49:30 +0530

    restored BlogData.js

---
+++ src/data/BlogData.js
@@ -0,0 +1,43 @@
+export const Blogs = [
+    {
+        id: 1,
+        name: "Basic Git cheat sheet",
+        tags: ["tutorial", "version-control", "git"],
+        date: "2 Jan, 2022",
+        imgSrc: "https://blogger.googleusercontent.com/img/a/AVvXsEio1KLgjdMcvR8G3MnlxeWsLnYUlqNueNa1TUWRNl7Nf5PRJ4NFH4WqAJ8IjsCUWj2tEKVsYYzyTtYxDTA4-s80o7p5pOxRYH2HTOWLns_eWjRxDr0_BLLYEha2Eg1tRTd6NjWeoBpCiiac38p_-RWxQBYvBSwLfxoykQcSQkqcwju4VIe3ps7QLBjF=w640-h268",
+        link: "https://fullstackstory.blogspot.com/2022/01/basic-git-cheat-sheet.html"
+    },
+    {
+        id: 2,
+        name: "Art of Shit Coding",
+        tags: ["coding", "tutorial", "life-hack"],
+        date: "30 Dec, 2021",
+        imgSrc: "https://miro.medium.com/max/875/1*erVqlTKQ7b1wnZhdBRMNKQ.jpeg",
+        link: "https://medium.com/@seek4samurai/art-of-shit-coding-fdf0a818b47"
+    },
+    {
+        id: 3,
+        name: "Dealing with Recursion as a Beginner",
+        tags: ["recursion", "data-structures"],
+        date: "26 Dec, 2021",
+        imgSrc: "https://images.pexels.com/photos/7691227/pexels-photo-7691227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        link: "https://dev.to/seek4samurai/dealing-with-recursion-as-a-beginner-2pfk"
+    },
+    {
+        id: 4,
+        name: "Web Developer, why become one?",
+        tags: ["react", "react-lottie", "styled-components"],
+        date: "6 July, 2021",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--TLod-spG--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/6sbv88f6910ski8annm2.jpg",
+        link: "https://dev.to/seek4samurai/web-developer-why-become-one-1n7h"
+    },
+
+    {
+        id: 5,
+        name: "How to create the UI that suits?",
+        tags: ["UI", "Design", "web-development"],
+        date: "24 Dec, 2021",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--c5O-m7UT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/45obl57qixvnehun6pxy.jpg",
+        link: "https://dev.to/seek4samurai/how-to-create-the-ui-that-suits-gki"
+    },
+]
